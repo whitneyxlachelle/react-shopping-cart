1178810f33378618c0c73c53c4491a9599498241
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-08 16:47:29 -0500

    created productcontext

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,7 @@
+//This object gives us two important components to work with 
+//Provider and Consumer
+import { createContext } from 'react';
+
+const ProductContext = createContext();
+
+export default ProductContext;
